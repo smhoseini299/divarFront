5a661c76b78d54279ba9c47e3717466a1136d5d9
smhoseini <smhoseini299@gmail.com>
2025-05-06 12:46:33 +0330

    edit css

--- app/components/ui/home/Sidebar.jsx
+++ app/components/ui/home/Sidebar.jsx
@@ -76,13 +76,13 @@ export default function Sidebar() {
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                         stroke="currentColor"
                                         className="h-6 w-6"
                                     >
                                         <path
                                             strokeLinecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinejoin="round"
                                             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                                         />
                                     </svg>
@@ -125,13 +125,13 @@ export default function Sidebar() {
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                         stroke="currentColor"
                                         className="h-6 w-6"
                                     >
                                         <path
                                             strokeLinecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinejoin="round"
                                             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                                         />
                                     </svg>
@@ -210,13 +210,13 @@ export default function Sidebar() {
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
-                                        stroke-width="1.5"
+                                        strokeWidth="1.5"
                                         stroke="currentColor"
                                         className="h-6 w-6"
                                     >
                                         <path
                                             strokeLinecap="round"
-                                            stroke-linejoin="round"
+                                            strokeLinejoin="round"
                                             d="M19.5 8.25l-7.5 7.5-7.5-7.5"
                                         />
                                     </svg>
---
+++ public/frontHtml/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirmatn: ["Vazirmatn"],
+      },
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
